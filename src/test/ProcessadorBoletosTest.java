66e0f4a6cbb31fda0e612cc5cbc4901c9be00f75
Victor Verdan <victorverdan@id.uff.br>
2022-04-18 14:28:08 -0300

    Iteração 1 de testAdicionarBoleto - teste que falha

--- src/test/ProcessadorBoletosTest.java
+++ src/test/ProcessadorBoletosTest.java
@@ -1,11 +1,24 @@
 package test;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import tdd_fatura_boleto.Boleto;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class ProcessadorBoletosTest {
 
-    @Test
-    public void testProcessarPagamento(){
+    private ProcessadorBoletos pb = new ProcessadorBoletos();
+    private ArrayList<Boleto> processamento;
 
+    @Test
+    public void testAdicionarBoleto(){
+
+        pb.adiciona(1, LocalDate.now(), "BOLETO", 500.00);
+        Assertions.assertFalse(processamento.isEmpty());
     }
+
 }
